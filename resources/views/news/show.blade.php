06fa20bf63da009cdb9d707654a5ee062150f142
Serds221 <basmakoffcerk@gmail.com>
2026-06-29 12:45:05 +0300

    fix: исправить ошибку 404 для favicon.svg и сделать обложку новости адаптивной 3:4 на мобильных

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -239,8 +239,8 @@
                 </div>
             @elseif($article->featured_image)
                 <!-- Featured Image cover -->
-                <div class="relative w-full h-[400px] rounded-[2rem] overflow-hidden mb-10 shadow-md border border-gray-200/50">
-                    <img src="{{ asset('storage/' . $article->featured_image) }}" class="w-full h-full object-cover" alt="{{ $article->title }}">
+                <div class="relative w-full aspect-[3/4] md:aspect-auto md:h-[400px] rounded-[2rem] overflow-hidden mb-10 shadow-md border border-gray-200/50 bg-gray-50 flex items-center justify-center">
+                    <img src="{{ asset('storage/' . $article->featured_image) }}" class="w-full h-full object-cover object-center" alt="{{ $article->title }}">
                 </div>
             @endif
 
